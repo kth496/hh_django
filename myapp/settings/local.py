4d7836f61b4d97b18fc49baec0321de8bc862783
yth0 <ythyth0@gmail.com>
2020-01-16 20:24:34 +0900

    add settings

---
+++ myapp/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'NAME': 'hh_testDB',
+        'USER': 'root',
+        'PASSWORD': 'password',
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'myapp.home',
+    'myapp.item'
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
+}
