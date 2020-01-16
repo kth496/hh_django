b04423c761eb8f055657132cfac15b727bfacd27
yth0 <ythyth0@gmail.com>
2020-01-16 18:02:58 +0900

    modify settings

--- myapp/settings/local.py
+++ myapp/settings/local.py
@@ -5,12 +5,21 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
-        'NAME': 'my_database',
-        'USER': 'root',
-        'PASSWORD': 'password',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'HOST': '127.0.0.1',
+#         'NAME': 'my_database',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#     }
+# }
+# INSTALLED_APPS = [
+#     'rest_framework',
+#     'item'
+# ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
