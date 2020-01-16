be5f9224af869e9f1c1e06bfef7ac202d4948db9
yth0 <ythyth0@gmail.com>
2020-01-16 20:13:22 +0900

    add settings to production.py

---
+++ myapp/settings/production.py
@@ -0,0 +1,29 @@
+import os
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': os.environ['MYSQL_ROOT_HOST'],
+        'NAME': os.environ['MYSQL_DATABASE'],
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_ROOT_PASSWORD'],
+    }
+}
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'myapp.home',
+    'myapp.item'
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50
+}
