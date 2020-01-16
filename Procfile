bc68157848994d20d1ccd1eccf3d9fc614255704
kth496 <ythyth0@gmail.com>
2020-01-16 21:24:37 +0900

    modify json data and settings

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: gunicorn myapp.wsgi --log-file -
 migrate: python manage.py migrate --settings=myapp.settings.production
-seed: python manage.py loaddata myapp/item/fixtures/items-data.json
+seed: python manage.py loaddata myapp/item/fixtures/modi-items-data.json
+seed: python manage.py loaddata myapp/item/fixtures/modi-ingred-data.json
--- myapp/item/models.py
+++ myapp/item/models.py
@@ -2,5 +2,18 @@ from django.db import models
 
 
 class Item(models.Model):
-    name = models.CharField(max_length=32)
-    quantity = models.IntegerField()
+    # id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    price = models.IntegerField()
+    gender = models.CharField(max_length=8)
+    category = models.CharField(max_length=20)
+    ingredients = models.TextField()
+    monthlySales = models.IntegerField()
+    imageId = models.TextField()
+
+
+class Ingredients(models.Model):
+    name = models.TextField()
+    oily = models.CharField(max_length=1)
+    dry = models.CharField(max_length=1)
+    sensitive = models.CharField(max_length=1)
