384970af14d87d86401310c2f01bdec7612c514c
kth496 <ythyth0@gmail.com>
2020-01-17 23:48:56 +0900

    update production settings

--- myapp/settings/production.py
+++ myapp/settings/production.py
@@ -19,11 +19,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'myapp.home',
     'myapp.item'
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 50,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
